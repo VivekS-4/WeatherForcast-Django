d830c9c4cf6fa723a8f4ff4053c9b2ac4b87edfc
Vivek <vivek.sharma@iitmjanakpuri.com>
2021-09-03 20:36:40 +0530

    Upload Media File

--- LookUp/urls.py
+++ LookUp/urls.py
@@ -3,8 +3,9 @@ from . import views
 #New page URL would be added here
 
 urlpatterns = [
-    path('', views.index, name = "index"),
-    # path('', views.Search, name = "Search"),
+    path('', views.Home, name = "Home"),
+    path('Home', views.index, name = "index"),
+    path('Results', views.Search, name = "Search"),
     path('About', views.About, name = "About"),
     path('./admin', views.Login, name = "Login"),
     path('accounts/', include('django.contrib.auth.urls')),
--- LookUp/views.py
+++ LookUp/views.py
@@ -2,23 +2,20 @@ from django.shortcuts import render
 import requests
 from .models import City
 from .forms import CityForm
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=9636be5ebf7c35a32203f1be39b63934'
 import json
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=9636be5ebf7c35a32203f1be39b63934'
+lat = str(28.37169)
+lg = str(77.056508)
+cc = "IN" #Country Code, Whenever going to user zipcode of another country change this here to that COUNTRY CODE
 
 def index(request):
-
     form = CityForm()
-    # city = 'Las Vegas'
     cities = City.objects.all()
     if request.method == 'POST': # only true if form is submitted
         form = CityForm(request.POST) # add actual request data to form for processing
         form.save() # will validate and save if validate
-
     weather_data = []
-
-
     for city in cities:
-
         city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
         weather = {
             'city' : city,
@@ -26,53 +23,32 @@ def index(request):
             'description' : city_weather['weather'][0]['description'],
             'icon' : city_weather['weather'][0]['icon']
         }
-    weather_data.append(weather)
-    context = {'weather_data' : weather_data, 'form': form}
-    return render(request, 'index.html', context) #returns the index.html template
+        weather_data.append(weather)
+    return render(request, 'index.html', {'weather_data' : weather_data, 'form': form}) #returns the index.html template
 
-
-
-def Home(request):
-	cities = City.objects.all() #return all the cities in the database
-	if request.method == 'POST': # only true if form is submitted
-		form = CityForm(request.POST) # add actual request data to form for processing
-		form.save()
-
-	form = CityForm()
-	weather_data = []
-	context = {'weather_data' : weather_data, 'form' : form}
-	for city in cities:
-		try:
-			city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
-			weather = {
-            'city' : city,
-            'temperature' : city_weather['main']['temp'],
-            'description' : city_weather['weather'][0]['description'],
-            'icon' : city_weather['weather'][0]['icon']
-			}
-		except Exception as e:
-			city_weather = " Weather Error"
-	
-	weather_data.append(weather) #add the data for the current city into our list
-	context = {'weather_data' : weather_data}
-	return render(request, 'Home.html', context) #returns the index.html template
 
 
 def Search(request):
-	Weather_request = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Delhi,IN&APPID=9636be5ebf7c35a32203f1be39b63934') 
-	air_request = requests.get('https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=28.621763&longitude=77.055823&distance=25&API_KEY=5C93347A-7199-4983-9D49-73D9F0DD49C3')
+    if request.method == "POST":
+        zipcode = request.POST.get("zipcode", False)
+        Weather_request = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+ zipcode +','+ cc +'&units=imperial&appid=b4baae3fd8dfcb64b615a38104ae65bc').json()
+        # requests.get('http://api.openweathermap.org/data/2.5/weather?q=New%20Delhi&units=imperial&appid=9636be5ebf7c35a32203f1be39b63934')
+        air_request = requests.get('https://api.weatherbit.io/v2.0/current/airquality?&postal_code='+ zipcode +'&key=7821ab051eb04daaa20e2c2fd19caec9') #valid till 02/10/21
+        try:
+            wth = (Weather_request)
+            api = json.loads(air_request.content)
+        except Exception as e :
+            wth = "Weather Error...",
+            api = "Air Quality Error"
+        return render(request, 'Search.html', {'api': api, 'wth': wth, 'zipcode': zipcode}) 
+    
 
-	try:
-		aqi = json.loads(Weather_request.content)
-		api = json.loads(air_request.content)
-	except Exception as e :
-		aqi = "Weather Error...",
-		api = "Air Quality Error"
-	return render(request, 'Search.html', {'api': api, 'aqi': aqi})
+
+def Home(request):
+    return render(request, 'Home.html', {})
 
 def About(request):
 	return render(request, 'About.html', {})
 
 def Login(request):
 	return render(request, 'admin', {})
-
